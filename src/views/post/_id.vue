22e3945fa4af482e5a4fa3998ddfc2bf4af6a149
ywlee <zestlee1106@gmail.com>
2020-07-19 17:27:19 +0900

    * post id page 디자인 수정

--- src/views/post/_id.vue
+++ src/views/post/_id.vue
@@ -1,7 +1,32 @@
 <template>
-  <div v-if="post">
-    <div>
-      {{ post.title }}
+  <div v-if="post" class="post-area">
+    <div class="post">
+      <div class="post-title">
+        {{ post.title }}
+      </div>
+      <hr />
+      <div class="post-content">
+        {{ post.body }}
+      </div>
+      <hr />
+      <div class="reply-box" v-if="commentList">
+        Reply
+        <span style="font-weight: 700;"> {{ commentList.length }}</span> 개
+      </div>
+      <div class="post-comment">
+        <div v-for="comment in commentList" :key="comment.id">
+          <hr />
+          <div class="comment-author">
+            <div class="comment-name">
+              {{ comment.name }}
+            </div>
+            <div class="comment-email">({{ comment.email }})</div>
+          </div>
+          <div class="comment-body">
+            {{ comment.body }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,7 +39,7 @@ export default {
   data() {
     return {
       post: {},
-      comments: null,
+      commentList: null,
       title: "front_gangnam"
     };
   },
@@ -24,10 +49,10 @@ export default {
   async mounted() {
     const { data: post } = await Post.getPost(this.$route.params.id);
     this.post = post;
-    const { data: comments } = await Post.getPostComments(
+    const { data: commentList } = await Post.getPostComments(
       this.$route.params.id
     );
-    this.comments = comments;
+    this.commentList = commentList;
   },
   methods: {
     ...mapMutations(["SET_TITLE"])
@@ -42,3 +67,95 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.post {
+  margin-top: 7vw;
+  background: #f7f7f7;
+  max-width: 529px;
+  width: 100%;
+}
+.post-area {
+  display: -ms-flexbox;
+  display: flex;
+  justify-content: center;
+}
+.post-title {
+  margin-top: 10px;
+  font-size: 2rem;
+  font-weight: 450;
+  color: #000000;
+  width: 80%;
+  display: inline-block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.post-content {
+  width: 90%;
+  display: inline-block;
+  font-size: 0.96rem;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.comment-author {
+  width: 90%;
+  display: flex;
+  display: -ms-flexbox;
+  padding-left: 3%;
+}
+.comment-name {
+  display: inline-block;
+  width: 35%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: 0.83rem;
+  font-weight: 600;
+  text-align: left;
+  color: #000000;
+}
+.comment-email {
+  display: inline-block;
+  text-align: left;
+  width: 50%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: 0.8rem;
+  font-weight: 480;
+  color: #4c4c4c;
+}
+.comment-body {
+  display: inline-block;
+  text-align: left;
+  font-size: 0.8rem;
+  color: #000000;
+  width: 93%;
+}
+
+.post-comment {
+  padding-bottom: 10px;
+}
+
+hr {
+  border: 0;
+  height: 1px;
+  width: 98%;
+  background-color: #a6a6a6;
+}
+
+.post-comment hr {
+  background-color: #d5d5d5;
+  height: 0.4px;
+  width: 96%;
+}
+.reply-box {
+  text-align: left;
+  margin-left: 10px;
+  color: #f9c900;
+  font-weight: 400;
+}
+</style>
